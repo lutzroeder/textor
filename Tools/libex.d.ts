d97c452ae010f1f124b824f8c09f5852d5175476
Lutz Roeder <lutzroeder@users.noreply.github.com>
2015-12-18 02:17:40 -0800

    Update to TypeScript 1.7.3

--- Tools/libex.d.ts
+++ Tools/libex.d.ts
@@ -1,51 +1,17 @@
 
 
-interface TouchEvent extends UIEvent {
-    touches: TouchList;
-    targetTouches: TouchList;
-    changedTouches: TouchList;
-    altKey: boolean;
-    metaKey: boolean;
-    ctrlKey: boolean;
-    shiftKey: boolean;
-    relatedTarget: EventTarget;
-}
-
-interface TouchList {
-    length: number;
-    item(index: number): Touch;
-    [index: number]: Touch;
-    identifiedTouch(identifier: number): Touch;
-}
-
-interface Touch {
-    identifier: number;
-    target: EventTarget;
-    screenX: number;
-    screenY: number;
-    clientX: number;
-    clientY: number;
-    pageX: number;
-    pageY: number;
-    radiusX: number;
-    radiusY: number;
-    rotationAngle: number;
-    force: number;
-}
-
-interface MouseWheelEvent
-{
-    axis: number;
-    wheelDeltaX: number;
-    wheelDeltaY: number;
-}
-
-interface MSStyleCSSProperties
-{
+interface CSSStyleDeclaration {
     resize: string;
 }
 
-interface DragEvent
-{
+interface Event {
+	wheelDelta: number;
+}
+
+interface DragEvent {
+    clipboardData: DataTransfer;
+}
+
+interface Window {
     clipboardData: DataTransfer;
 }
